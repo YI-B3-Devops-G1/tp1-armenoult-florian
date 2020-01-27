b574e5d070761aa94ebfcbef7c86c6c3e8ca8515
Floo42 <florian.a@live.fr>
2020-01-27 15:30:39 +0100

    TP1 Vagrant

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sed --in-place s/us/fr/g /etc/default/keyboard
+apt-get update
+apt-get -y install nodejs openssh-server nginx
+rm /var/www/html/*
+mv /tmp/index.html /var/www/html/index.html
+reboot
